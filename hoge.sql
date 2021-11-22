cd71cc0f64dc79b086024ee755ccfaf758af1f33
code560 <smartphone.560+git@gmail.com>
2021-11-23 00:42:37 +0900

    add aggregate

--- hoge.sql
+++ hoge.sql
@@ -1,6 +1,6 @@
 WITH 
 Const as (
-  SELECT '1981-03-01' as targetDate
+  SELECT '1981-06-01' targetDate
 )
 
 ,AsaNumber as (
@@ -9,25 +9,25 @@ Const as (
   FROM (
     SELECT 
       ROW_NUMBER() OVER() _no,
-      id,"action" as _act,
+      id,"action" _act,
       CASE
         WHEN "action" == '!mod' THEN date2
         ELSE date1
-      END as _date,
+      END _date,
       CASE
         WHEN value4 <> '' THEN value4
         WHEN value3 <> '' THEN value3
         WHEN value2 <> '' THEN value2
         WHEN value1 <> '' THEN value1
         ELSE NULL
-      END as _val,
+      END _val,
       CASE
         WHEN value4 <> '' THEN 'value4'
         WHEN value3 <> '' THEN 'value3'
         WHEN value2 <> '' THEN 'value2'
         WHEN value1 <> '' THEN 'value1'
         ELSE NULL
-      END as _name
+      END _name
     FROM -
   )
   WHERE _val is not NULL AND _name is not NULL
@@ -36,42 +36,71 @@ Const as (
 ,AsDefined as (
   SELECT 
     ROW_NUMBER() OVER() _no, 
-    "name" as _name, 
-    code as _code, 
-    val as _val,
-    "name"||'-'||val as _planId
+    "name" _name, 
+    code _code, 
+    val _val
   FROM .\def\plans.csv
 )
 
 ,AsaCode as (
 SELECT
-  p._no, p._name, p._code, p._val, COALESCE(a.月数, 0) as 月数
-  -- p._planId, COALESCE(a.月数, 0) as 月数
-FROM AsDefined as p 
-  LEFT JOIN (
+  _no, _name, _code, _val
+  ,COALESCE(( 
     SELECT
-      _name,
-      _val,
-      COUNT(*) as 月数
+      _count
     FROM (
-      SELECT 
-        MAX(_no) as no,
-        id,
-        _act,
-        _date,
-        _name,
-        _val
-      FROM AsaNumber, Const
-      WHERE _date <= date(Const.targetDate, '+1 months','start of month') AND _act <> 'fin'
-      GROUP BY id
+      SELECT _name, _val, COUNT(*) _count
+      FROM (
+        SELECT MAX(_no), _name, _val
+        FROM AsaNumber, Const
+        WHERE _date < date(Const.targetDate,'-1 months','start of month') AND _act <> 'fin'
+        GROUP BY id
+      ) as a
+      WHERE p._name = a._name AND p._val = a._val
+      GROUP BY _name, _val
     )
-    GROUP BY _name, _val
-  ) as a
-  ON p._name = a._name AND p._val = a._val
+  ), 0) 月数1
+  ,COALESCE(( 
+    SELECT
+      _count
+    FROM (
+      SELECT _name, _val, COUNT(*) _count
+      FROM (
+        SELECT MAX(_no), _name, _val
+        FROM AsaNumber, Const
+        WHERE _date < date(Const.targetDate,'start of month') AND _act <> 'fin'
+        GROUP BY id
+      ) as a
+      WHERE p._name = a._name AND p._val = a._val
+      GROUP BY _name, _val
+    )
+  ), 0) 月数2
+  ,COALESCE(( 
+    SELECT
+      _count
+    FROM (
+      SELECT _name, _val, COUNT(*) _count
+      FROM (
+        SELECT MAX(_no), _name, _val
+        FROM AsaNumber, Const
+        WHERE _date < date(Const.targetDate,'+1 months','start of month') AND _act <> 'fin'
+        GROUP BY id
+      ) as a
+      WHERE p._name = a._name AND p._val = a._val
+      GROUP BY _name, _val
+    )
+  ), 0) 月数3
+FROM AsDefined as p
 )
 
+,AsAggregate as (
+SELECT
+  _name, _code, _val, 月数1, 月数2, 月数3, (月数1 + 月数2 + 月数3) 合計, ((月数1 + 月数2 + 月数3) * _val) allVal
+FROM AsaCode
+)
 
 -- select * from Const
 -- select * from AsaNumber
 -- select * from AsDefined
-select * from AsaCode
+-- select * from AsaCode
+select * from AsAggregate
